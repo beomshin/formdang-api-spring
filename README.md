682b08eb9dc29e785d1b90ea67c92b6cbf9260d1
beomshin.kim <chzh859@naver.com>
2023-11-09 21:59:35 +0900

    # 헬스 체크

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# fromdang-sp-was
-fromdang-sp-was
+## 프로젝트 구성
+
+- formdang-api
+  - api 모듈
+
+## 프로젝트 실행
+
+- 디비 접근 가능 상태 처리 (AWS 방화벽 설정)
+
+## 빌드 및 배포 방법
+
+- package 수행 후 jar 파일 생성
+- 
+## 기타
---
+++ formdang-api/src/main/java/com/kr/formdang/FormdangApiApplication.java
@@ -0,0 +1,13 @@
+package com.kr.formdang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormdangApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormdangApiApplication.class, args);
+    }
+
+}
