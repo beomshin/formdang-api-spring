8775dec143943f23e1b3f89e39d75e82116294c7
beomshin.kim <chzh859@naver.com>
2023-11-17 23:09:04 +0900

    # 스크립트 구현중

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# APP_NAME=web-api
-# REPOSITORY=/home/sp/web
-#
-# echo "> Check the currently running container"
-# CONTAINER_ID=$(docker ps -aqf "name=$APP_NAME")
-#
-# if [ -z "$CONTAINER_ID" ];
-# then
-#   echo "> No such container is running."
-# else
-#   echo "> Stop and remove container: $CONTAINER_ID"
-#   docker stop "$CONTAINER_ID"
-#   docker rm "$CONTAINER_ID"
-# fi
-#
-# echo "> Remove previous Docker image"
-# docker rmi "$APP_NAME"
-#
-# echo "> Build Docker image"
-# docker build -t "$APP_NAME" "$REPOSITORY"
-#
-# echo "> Run the Docker container"
-# docker run -d -p 8080:8080 --name "$APP_NAME" "$APP_NAME"
+cd /home/sp/deploy/web
 
-systemctl stop formdang-sp-was.service
+# app.jar 파일이 있는지 확인
+if [ -e app.jar ]; then
+    # app.jar 파일이 있다면 해쉬값 계산
+    hash_value=$(md5sum app.jar | awk '{ print $1 }')
 
-systemctl start formdang-sp-was.service
+    # backup 디렉토리 생성 (이미 있다면 무시)
+    mkdir -p backup
+
+    # 백업 파일 경로 설정
+    backup_file="backup/app-${hash_value}.jar"
+
+    # 백업 실행
+    cp app.jar "$backup_file"
+
+    echo "백업이 완료되었습니다. 백업 파일: $backup_file"
+
+else
+    echo "app.jar 파일이 현재 디렉토리에 존재하지 않습니다."
+fi
+
+
+cp /home/sp/source/web/web-api/target/*.jar app.jar
+
+echo "app.jar 생성 완료"
