ffe4f1c269bacb7a78af3601b51275802601a953
chzh859@naver.com <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>
2023-11-18 21:23:35 +0900

    # 기본 구성 완료

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# formdang-sp-was 서비스의 상태를 확인
+service_status=$(systemctl is-active formdang-sp-was)
+
+# formdang-sp-was 서비스가 실행 중인 경우
+if [ "$service_status" == "active" ]; then
+    # formdang-sp-was 서비스를 중지
+    systemctl stop formdang-sp-was
+else
+    echo "formdang-sp-was 서비스는 이미 중지되어 있습니다."
+fi
+
 cd /home/sp/deploy/web
 
 # app.jar 파일이 있는지 확인
@@ -24,18 +35,6 @@ else
 fi
 
 cp /home/sp/source/web/web-api/target/*.jar app.jar
-
-# formdang-sp-was 서비스의 상태를 확인
-service_status=$(systemctl is-active formdang-sp-was)
-
-# formdang-sp-was 서비스가 실행 중인 경우
-if [ "$service_status" == "active" ]; then
-    # formdang-sp-was 서비스를 중지
-    systemctl stop formdang-sp-was
-else
-    echo "formdang-sp-was 서비스는 이미 중지되어 있습니다."
-fi
-
 
 # formdang-sp-was 서비스를 시작
 systemctl start formdang-sp-was
