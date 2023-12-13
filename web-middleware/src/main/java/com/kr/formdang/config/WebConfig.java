c138eae3be06a1c0bcb6d80cc7d9777e163c5a1c
beomshin.kim <chzh859@naver.com>
2023-12-13 22:38:42 +0900

    # CORS 설정 테스트

--- web-middleware/src/main/java/com/kr/formdang/config/WebConfig.java
+++ web-middleware/src/main/java/com/kr/formdang/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.kr.formdang.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,4 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
         WebMvcConfigurer.super.addFormatters(registry);
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
 }
